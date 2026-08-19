b9583f407ce723666d2b2e2724628d66e8b04ca4
shubhxitachi-afk <shubhxitachi@gmail.com>
2026-08-19 23:40:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = "34829388"
 API_HASH = "30df7fd725bd39aa2e3b7a55b15a182b"
 BOT_TOKEN = "8954458459:AAHRTJucDx31wUKbCQhF0ICzJ4pUtN5_VuE"
-MONGO_URL = "MONGO_URL = "mongodb+srv://shubhxitachi_db_user:3r1jKiHGmAyd93wy@nezukonbot.wkzudm6.mongodb.net/?appName=nezukonbot&tlsAllowInvalidCertificates=true"
+MONGO_URL = "mongodb+srv://shubhxitachi_db_user:3r1jKiHGmAyd93wy@nezukonbot.wkzudm6.mongodb.net/?appName=nezukonbot&tlsAllowInvalidCertificates=true"
 OWNER_ID = "8888788314"
 SUPPORT_GRP = "@nezukobotupdatesx1"
 UPDATE_CHNL = "@nezukobotupdatesx1"
